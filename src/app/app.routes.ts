87378a037887babfabc29af30ad6c094d2d2e0f8
saieesha <saieeshabonthala@gmail.com>
2024-09-18 11:05:39 -0500

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,7 @@ import { Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 
-
 export const routes: Routes = [
-    { path: '', component: FirstComponent },
-    { path: 'second', component: SecondComponent },
+    {path: '', component: FirstComponent},
+    {path: 'second', component: SecondComponent},
 ];
